c2ad3fb07d5589042dc7b2e01acd1358387fbcd8
Caleb Larsen <caleb.larsen@rentvision.com>
2025-07-07 13:36:49 -0500

    Mark OrderUtil::calculateTotal as @Deprecated, since the method should be used on the Order class directly.

--- app/src/main/java/org/refactoring/example1/after/OrderUtil.java
+++ app/src/main/java/org/refactoring/example1/after/OrderUtil.java
@@ -1,6 +1,14 @@
 package org.refactoring.example1.after;
 
 public class OrderUtil {
+    /**
+     * Calculates the total cost for the specified order, including tax, discounts, and shipping.
+     *
+     * @param order the order for which the total cost is calculated
+     * @return the total cost of the order as a double value
+     * @deprecated This method is deprecated. Use {@link Order#calculateTotal()} directly.
+     */
+    @Deprecated
     public static double calculateTotal(Order order) {
         return order.calculateTotal();
     }
